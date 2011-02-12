c576215e1cda17c188a12f5079052e741a152659
Osledy Bazo <osledybazo@gmail.com>
2011-02-12 17:03:19 -0430

    parte 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Urlsh::Application.routes.draw do
+  
+  resources :urls, :only => [:show, :new, :create] 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +52,7 @@ Urlsh::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => redirect('/urls/new')  
 
   # See how all your routes lay out with "rake routes"
 
